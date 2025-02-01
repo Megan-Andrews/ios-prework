a2bd850b11b32697b8de5ebe2340333178ee711a
Megan Andrews <megan.r.andrews@gmail.com>
2025-01-31 19:14:17 -0800

    prework app

--- ios-prework/ViewController.swift
+++ ios-prework/ViewController.swift
@@ -9,6 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labels: [UILabel]!
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    @IBAction func changeTextColor(_ sender: UISwitch) {
+        let randomColor = changeColor()
+            if let labels = labels {  // Ensure labels exist
+                    for label in labels {
+                        label.textColor = randomColor
+                    }
+                } else {
+                    print("Labels array is nil!")
+                }
+    }
+    
+    func changeColor() -> UIColor{
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
